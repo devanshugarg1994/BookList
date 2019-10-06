f15e96fd43bb870f92359ec5eb9cb879266a433a
devanshu <devanshugarg1994@gmail.com>
2019-10-06 19:26:58 +0530

    basic setup

---
+++ src/Ui.js
@@ -0,0 +1,44 @@
+import {Book} from './Book';
+// No need to intatiante the class using static method
+export default class UI {
+    //all methods are static
+
+    //using hardcode data intially
+
+    static displayBooks () {
+        const StoredBooks = [
+            {
+                title: "Book-one",
+                author: "John Doe",
+                isbn: '3434434'   
+            },
+            {
+                title: "Book-two",
+                author: "Jan Doe",
+                isbn: '56456'   
+            }
+        
+         ];
+         console.log("reached");
+
+         const books = StoredBooks;
+         books.forEach(book => { 
+             UI.addBookToList(book);
+         });     
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href = "#" class = "btn btn-danger btn-sm delete"></a>Delete</td>
+        `;
+
+        list.appendChild(row);
+    }
+
+}
